a28b5536503341269871622c7f6e130ec83d8067
Sarvesh Yadav <sarveshyadav1v@gmail.com>
2024-10-26 17:16:44 +0530

    vercel deploy issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import Herosection from "./components/herosection"
+import Herosection from "./components/Herosection"
 export default function App() {
   return (
     <Herosection/>
